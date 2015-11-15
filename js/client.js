f27d91c4657e033e14f8f484fae27a1569afb860
Helena Mosbæk <helenalm@live.dk>
2015-11-15 21:19:36 +0100

    draft for registerMenu and userMenu Updated loginMenu client.js file where the js-magic is gonna happen

--- js/client.js
+++ js/client.js
@@ -1,3 +1,17 @@
-/**
- * Created by helenamosbaek on 12/11/15.
- */
+$(document).ready(function () {
+
+    $("#button").click(function () {
+
+
+        $.ajax({
+            type: "GET",
+            url: "http://localhost:9998/api/user",
+            success: function (msg) {
+                console.log(msg);
+            }
+        });
+
+    });
+
+
+});
