2b2a9f8d92197845239188b9d127482104f7e919
Helena Mosbæk <helenalm@live.dk>
2015-11-16 22:26:05 +0100

    a validationform for the registerform, that checks that the user information is correct. It also suggests a username, based on first and last name.

--- js/client.js
+++ js/client.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    $("#button").click(function () {
+    $("#playNow").click(function () {
 
 
         $.ajax({
@@ -13,5 +13,59 @@ $(document).ready(function () {
 
     });
 
+    $("#registerForm").validate({
+        rules: {
+            firstName: "required",
+            lastName: "required",
+            userName: {
+                required: true,
+                minlenght: 2
+            },
+            password: {
+                required: true,
+                minlenght: 4
+            },
+            confirm_password: {
+                required: true,
+                minlenght: 4,
+                equalTo: "#password"
+            },
+            email: {
+                required: true,
+                email: true
+            },
+        },
+        messages: {
+            firstName: "Woops! Enter your first name",
+            lastName: "Woops! Enter your last name",
+            userName: {
+                required: "Woops! Enter a username",
+                minlenght: "Woops! The username must be at least 2 characters"
+            },
+            password: {
+                required: "Woops! Enter a password",
+                minlenght: "Woops! The password must be at least 4 characters"
+            },
+            confirm_password: {
+                required: "Woops! Confirm your password",
+                minlenght: "Woops! Your password must be at least 4 characters",
+                equalTo: "Woops! Did you already forget your password?"
+            },
+            email: {
+                required: "Woops! Enter an email",
+                email: "Woops! Enter a REAL email"
+            }
+        }
+    });
+
+    $("#userName").focus(function() {
+        var firstName = $("#firstName").val();
+        var lastName = $("#lastName").val();
+        if (firstName && lastName && !this.value) {
+            this.value = firstName + lastName;
+        }
+    });
 
 });
+
+$("#registerForm").validator();
